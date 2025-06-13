63061cbec40d6d0a72e916f1cb1d4135af24ba8d
Javier Noguer Fernández <jnoguerfernandez@gmail.com>
2025-06-13 16:41:32 +0200

    Inserción de datos en archivo data.sql

---
+++ src/main/resources/templates/data.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO projects (id, name, description) VALUES
+(1, 'Rediseño de Sitio Web', 'Renovación completa del sitio web corporativo'),
+(2, 'App Móvil de Clientes', 'Desarrollo de aplicación para iOS y Android'),
+(3, 'Migración a la Nube', 'Traslado de infraestructura a AWS'),
+(4, 'Campaña Navideña', 'Promociones de fin de año'),
+(5, 'Capacitación DevOps', 'Formación en herramientas CI/CD');
